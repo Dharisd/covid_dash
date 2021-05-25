c374d4ec4201e6dcff9b164157fa3b3472ca3027
dharisd <dharisd@gmail.com>
2021-05-25 10:22:18 +0000

    updated admin site

--- dash/admin.py
+++ dash/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import ActiveCase,SampleDetail,QuarantineAddress
 
 
+#set council branding
+admin.site.site_title = "Covid19 Dashboard"
+admin.site.site_header = 'Covid19 Dashboard'
+admin.site.index_title = "Edit Dashboard"
+
+
 # Register your models here.
 admin.site.register(ActiveCase)
 admin.site.register(SampleDetail)
-admin.site.register(QuarantineAddress)
+
+@admin.register(QuarantineAddress)
+class QuarantineAddressAdmin(admin.ModelAdmin):
+    list_display = ("name","reason")
